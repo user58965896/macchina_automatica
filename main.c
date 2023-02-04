a7bb3752ad6ab672f4a78cc8c1f646e21484497b
user58965896 <124411526+user58965896@users.noreply.github.com>
2023-02-04 03:05:57 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,536 @@
+/**
+  ******************************************************************************
+  * @file    Proximity/Src/main.c 
+  * @author  MCD Application Team
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2017 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/** @addtogroup STM32L4xx_HAL_Applications
+  * @{
+  */
+
+/** @addtogroup Proximity
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+
+
+//DISTANZA MINIMA DI SICUREZZA
+#define MAX_DISTANCE 250
+
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+extern I2C_HandleTypeDef hI2cHandler;
+VL53L0X_Dev_t Dev =
+{
+  .I2cHandle = &hI2cHandler,
+  .I2cDevAddr = PROXIMITY_I2C_ADDRESS
+};
+
+extern UART_HandleTypeDef hDiscoUart;
+#ifdef __GNUC__
+/* With GCC/RAISONANCE, small msg_info (option LD Linker->Libraries->Small msg_info
+   set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#define GETCHAR_PROTOTYPE int __io_getchar(void)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#define GETCHAR_PROTOTYPE int fgetc(FILE *f)
+#endif /* __GNUC__ */
+
+/* Private function prototypes -----------------------------------------------*/
+static void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void VL53L0X_PROXIMITY_MspInit(void);
+static uint16_t VL53L0X_PROXIMITY_GetDistance(void);
+static void VL53L0X_PROXIMITY_Init(void);
+
+
+
+/* Private functions ---------------------------------------------------------*/
+
+void spegni_motori();
+void vai_avanti();
+void vai_indietro();
+void frena();
+int linea_nera_centrale();
+int linea_nera_sx();
+int linea_nera_dx();
+void gira_orario(int* centrale, int* sx);
+void gira_antiorario(int* centrale, int* dx);
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+
+  /* STM32L4xx HAL library initialization:
+       - Configure the Flash prefetch, Flash preread and Buffer caches
+       - Systick timer is configured by default as source of time base, but user 
+             can eventually implement his proper time base source (a general purpose 
+             timer for example or other time source), keeping in mind that Time base 
+             duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and 
+             handled in milliseconds basis.
+       - Low Level Initialization
+     */
+  HAL_Init();
+
+  /* Configure the System clock to have a frequency of 80 MHz */
+  SystemClock_Config();
+  MX_GPIO_Init();
+
+  /* Configure the User LED */
+  BSP_LED_Init(LED2); 
+  
+  /* Configure the User Button in GPIO Mode */
+  BSP_PB_Init(BUTTON_USER, BUTTON_MODE_GPIO);
+  
+  /* Initialize all configured peripherals */
+  hDiscoUart.Instance = DISCOVERY_COM1; 
+  hDiscoUart.Init.BaudRate = 115200;
+  hDiscoUart.Init.WordLength = UART_WORDLENGTH_8B;
+  hDiscoUart.Init.StopBits = UART_STOPBITS_1;
+  hDiscoUart.Init.Parity = UART_PARITY_NONE;
+  hDiscoUart.Init.Mode = UART_MODE_TX_RX;
+  hDiscoUart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  hDiscoUart.Init.OverSampling = UART_OVERSAMPLING_16;
+  hDiscoUart.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  hDiscoUart.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+
+  BSP_COM_Init(COM1, &hDiscoUart);
+
+  //Inizializzo i motori
+  spegni_motori();
+
+  //Inizializzo il sensore di prossimità;
+  VL53L0X_PROXIMITY_Init();
+  uint16_t distanza;
+
+  //Variabili linee
+  int linea_centrale = 0;
+  int linea_sx       = 0;
+  int linea_dx       = 0;
+
+
+  while (1)
+  {
+	 //Se la distanza non è sicura mi fermo
+	 distanza = VL53L0X_PROXIMITY_GetDistance();
+	 if( distanza < MAX_DISTANCE ){
+		spegni_motori();
+		printf("DISTANCE is = %d mm \n\r", distanza);
+		HAL_Delay(100);
+		continue;
+	}
+
+	//Se il sensore trova linea nera, vado avanti
+	if( linea_nera_centrale() ){
+		vai_avanti();
+	}else{
+		//Se non trovo la linea nera centrale allora controllo ai lati
+		if( linea_nera_sx() ){
+			//Mi riposiziono per eccitare il sensore centrale girando in senso orario
+			gira_orario(&linea_centrale, &linea_sx);
+		}else if( linea_nera_dx() ){
+			//Mi riposiziono per eccitare il sensore centrale girando in senso antiorario
+			gira_antiorario(&linea_centrale, &linea_dx);
+		//Se non c'è alcuna linea mi fermo
+		}else{
+			frena();
+			spegni_motori();
+		}
+	}
+
+
+
+	HAL_Delay(10);
+  }
+}
+
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3|GPIO_PIN_4, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1|GPIO_PIN_4, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA2 PA7 PA15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_7|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA3 PA4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB1 PB4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (MSI)
+  *            SYSCLK(Hz)                     = 80000000
+  *            HCLK(Hz)                       = 80000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 1
+  *            APB2 Prescaler                 = 1
+  *            MSI Frequency(Hz)              = 4000000
+  *            PLL_M                          = 1
+  *            PLL_N                          = 40
+  *            PLL_R                          = 2
+  *            PLL_P                          = 7
+  *            PLL_Q                          = 4
+  *            Flash Latency(WS)              = 4
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* MSI is enabled after System reset, activate PLL with MSI as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+  RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 40;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLP = 7;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    /* Initialization Error */
+    while(1);
+  }
+  
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;  
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    /* Initialization Error */
+    while(1);
+  }
+}
+
+/**
+  * @brief  Test of VL53L0X proximity sensor.
+  */
+
+
+/**
+  * @brief  VL53L0X proximity sensor Initialization.
+  */
+static void VL53L0X_PROXIMITY_Init(void)
+{
+  uint16_t vl53l0x_id = 0; 
+  VL53L0X_DeviceInfo_t VL53L0X_DeviceInfo;
+  
+  /* Initialize IO interface */
+  SENSOR_IO_Init();  
+  VL53L0X_PROXIMITY_MspInit();
+  
+  memset(&VL53L0X_DeviceInfo, 0, sizeof(VL53L0X_DeviceInfo_t));
+  
+  if (VL53L0X_ERROR_NONE == VL53L0X_GetDeviceInfo(&Dev, &VL53L0X_DeviceInfo))
+  {  
+    if (VL53L0X_ERROR_NONE == VL53L0X_RdWord(&Dev, VL53L0X_REG_IDENTIFICATION_MODEL_ID, (uint16_t *) &vl53l0x_id))
+    {
+      if (vl53l0x_id == VL53L0X_ID)
+      {
+        if (VL53L0X_ERROR_NONE == VL53L0X_DataInit(&Dev))
+        {
+          Dev.Present = 1;
+          SetupSingleShot(Dev);
+        }
+        else
+        { 
+          printf("VL53L0X Time of Flight Failed to send its ID!\n\r");
+        }
+      }
+    }
+    else
+    {
+      printf("VL53L0X Time of Flight Failed to Initialize!\n\r");
+    }
+  }
+  else
+  {
+    printf("VL53L0X Time of Flight Failed to get infos!\n\r");
+  }  
+}
+
+/**
+  * @brief  Get distance from VL53L0X proximity sensor.
+  * @retval Distance in mm
+  */
+static uint16_t VL53L0X_PROXIMITY_GetDistance(void)
+{
+  VL53L0X_RangingMeasurementData_t RangingMeasurementData;
+  
+  VL53L0X_PerformSingleRangingMeasurement(&Dev, &RangingMeasurementData);
+  
+  return RangingMeasurementData.RangeMilliMeter;  
+}
+
+/**
+  * @brief  VL53L0X proximity sensor Msp Initialization.
+  */
+static void VL53L0X_PROXIMITY_MspInit(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct;
+  
+  /*Configure GPIO pin : VL53L0X_XSHUT_Pin */
+  GPIO_InitStruct.Pin = VL53L0X_XSHUT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(VL53L0X_XSHUT_GPIO_Port, &GPIO_InitStruct);
+  
+  HAL_GPIO_WritePin(VL53L0X_XSHUT_GPIO_Port, VL53L0X_XSHUT_Pin, GPIO_PIN_SET);
+  
+  HAL_Delay(1000);  
+}
+
+/**
+  * @brief  Gets numeric values from the Hyperterminal.
+  * @param  Maximum value allowed (value)
+  * @retval The character received
+  */
+uint32_t Serial_Scanf(uint32_t value)
+{
+  uint16_t tmp = 0;
+  
+  tmp = getchar();
+  
+  if (tmp > value)
+  {
+    printf("\n\r\r  !!! Please enter valid number between 0 and %lu \n\r", value);
+    return 0xFF;
+  }
+  return tmp;
+}
+
+/**
+  * @brief Retargets the C library msg_info function to the USART.
+  * @param None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the serial port and Loop until the end of transmission */
+  while (HAL_OK != HAL_UART_Transmit(&hDiscoUart, (uint8_t *) &ch, 1, 30000))
+  {
+    ;
+  }
+  return ch;
+}
+
+/**
+  * @brief Retargets the C library scanf function to the USART.
+  * @param None
+  * @retval None
+  */
+GETCHAR_PROTOTYPE
+{
+  /* Place your implementation of fgetc here */
+  /* e.g. readwrite a character to the USART2 and Loop until the end of transmission */
+  uint8_t ch = 0;
+  while (HAL_OK != HAL_UART_Receive(&hDiscoUart, (uint8_t *)&ch, 1, 30000))
+  {
+    ;
+  }
+  return ch;
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+void Error_Handler(void)
+{  
+  /* User can add his own implementation to report the HAL error return state */
+  printf("!!! ERROR !!!\n\r");
+  while(1) 
+  {
+  }
+}
+
+
+void spegni_motori(){
+	//IMPOSTO TUTTI I BIT A 0
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
+
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+}
+
+void vai_indietro(){
+	  //CONFIGURAZIONE PRIMA RUOTA
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
+
+	  //CONFIGURAZIONE SECONDA RUOTA
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
+}
+
+void vai_avanti(){
+	  //IMPOSTO I BIT DELLA PRIMA RUOTA
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
+
+	  //IMPOSTO I BIT DELLA SECONDA RUOTA
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+}
+
+void frena(){
+	  //CONFIGURAZIONE CON TUTTI I BIT A 1
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
+
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
+}
+
+int linea_nera_centrale(){
+	//RITORNO 1 SE IL SENSORE CENTRALE HA TROVATO LA LINEA NERA
+	return  (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_15) == GPIO_PIN_SET ) ? 1:0;
+}
+
+int linea_nera_sx(){
+	//RITORNO 1 SE IL SENSORE SINISTRO HA TROVATO LA LINEA NERA
+	return  (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_2) == GPIO_PIN_SET ) ? 1:0;
+}
+
+int linea_nera_dx(){
+	//RITORNO 1 SE IL SENSORE DESTRO HA TROVATO LA LINEA NERA
+	return  (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_7) == GPIO_PIN_SET ) ? 1:0;
+}
+
+void gira_orario(int* centrale, int* sx){
+
+	//La ruota dx va in antiorario
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
+
+	//La ruota sx va in orario
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+
+
+	  //Finché la macchina non rileva una linea al centro
+	  //continua a girare
+	  while(!linea_nera_centrale() && linea_nera_sx())
+		  HAL_Delay(10);
+	  //Trovata la linea mi fermo e spengo i motori
+	  frena();
+	  spegni_motori();
+
+	  //Imposto le variabili
+	  *centrale = 1;
+	  *sx = 0;
+}
+
+void gira_antiorario(int* centrale, int* dx){
+
+	//La ruota dx va in orario
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
+
+	//La ruota sx va in antiorario
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
+
+	  //Finché la macchina non rileva una linea al centro
+	  //continua a girare
+	  while(!linea_nera_centrale() && linea_nera_dx())
+	  		  HAL_Delay(10);
+	  //Trovata la linea mi fermo e spengo i motori
+	  frena();
+	  spegni_motori();
+
+	  //Imposto le variabili
+	  *centrale = 1;
+	  *dx = 0;
+}
+
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n\r", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
